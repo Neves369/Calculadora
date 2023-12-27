8134af7d0f1c929a5446b06618b59b2084b74628
Neves369 <douglasbrian369@hotmail.com>
2023-12-27 16:26:22 -0300

    finalização do metodo de executar operação

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
 
 export default function App() {
 
-  const [displayValue, setDisplayValue] = useState('0');
+  const [current, setCurrent] = useState(0);
+  const [values, setValues] = useState([0,0]);
+  const [operation, setOperation] = useState(null);
+  const [displayValue, setDisplayValue] = useState("0");
+  const [clearDisplay, setClearDisplay] = useState(false);
 
   const addDigit = (n) =>{
-    setDisplayValue(n)
+    if(n == "." && displayValue.includes(".")){
+      return
+    }
+
+    const isClearDisplay = displayValue == '0' || clearDisplay
+    const currentValue = isClearDisplay ? "" : displayValue
+    const isDisplayValue = currentValue + n
+
+    setDisplayValue(isDisplayValue)
+    setClearDisplay(false)
+
+    if(n !== "."){
+      const newValue = parseFloat(isDisplayValue)
+      const nValues = [...values]
+      nValues[current] = newValue
+      setValues(nValues)
+    }
+
   }
 
   const clearMemory = () =>{
-    setDisplayValue('0')
+    setCurrent(0);
+    setValues([0,0]);
+    setOperation(null);
+    setDisplayValue("0");
+    setClearDisplay(false);
   }
 
-  const setOperation = (operation) =>{
-    setDisplayValue('0')
+  const selOperation = (nOperation) =>{
+    if(current === 0){
+      setOperation(nOperation)
+      setCurrent(1)
+      setClearDisplay(true)
+    }
+    else{
+      const equals = nOperation === "="
+      const nValues = [...values]
+      try {
+        nValues[0] = eval(`${nValues[0]} ${operation} ${nValues[1]}`)
+      } catch (error) {
+        nValues = values[0]
+      }
+
+      nValues[1] = 0
+      setDisplayValue(nValues[0])
+      setOperation(equals? null : nOperation)
+      setCurrent(equals? 0 : 1)
+      setClearDisplay(!equals)
+      setValues(nValues)
+    }
+    // setDisplayValue('0')
   }
 
   return (
@@ -25,22 +71,22 @@ export default function App() {
       <Display value={displayValue}/>
       <View style={styles.buttons}>
         <Button triple label="AC" onPress={clearMemory}/>
-        <Button operation label="/" onPress={setOperation}/>
+        <Button operation label="/" onPress={selOperation}/>
         <Button label="7" onPress={addDigit}/>
         <Button label="8"  onPress={addDigit}/>
         <Button label="9"  onPress={addDigit}/>
-        <Button operation label="*"  onPress={setOperation}/>
+        <Button operation label="*"  onPress={selOperation}/>
         <Button label="4"  onPress={addDigit}/>
         <Button label="5"  onPress={addDigit}/>
         <Button label="6"  onPress={addDigit}/>
-        <Button operation label="-" onPress={setOperation}/>
+        <Button operation label="-" onPress={selOperation}/>
         <Button label="1"  onPress={addDigit}/>
         <Button label="2"  onPress={addDigit}/>
         <Button label="3"  onPress={addDigit}/>
-        <Button operation label="+" onPress={setOperation}/>
+        <Button operation label="+" onPress={selOperation}/>
         <Button double label="0"  onPress={addDigit}/>
         <Button label="." onPress={addDigit}/>
-        <Button label="=" onPress={setOperation}/>
+        <Button label="=" onPress={selOperation}/>
       </View>
     </View>
   );
